2759c743966a2047344a148bfe613e912b788dd3
Andrea Josefina Montoya Ruiz <andrea.montoya@ed.company>
2021-08-04 22:03:21 -0300

    enhance: melhoras com async pipe

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,29 +1,16 @@
 import { AcoesService } from './acoes.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Acoes } from './modelo/acoes';
-import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'rxjs-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.scss'],
 })
-export class AcoesComponent implements OnInit, OnDestroy {
+export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes: Acoes;
-  private subscription: Subscription;
+  acoes$ = this.acoesService.getcoes();
 
   constructor(private acoesService: AcoesService) { }
-
-  ngOnInit() {
-   this.subscription = this.acoesService.getcoes()
-      .subscribe((acoes) => {
-        this.acoes = acoes;
-      });
-  }
-
-  ngOnDestroy(){
-    this.subscription.unsubscribe();
-  }
 }
